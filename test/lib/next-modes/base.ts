a70f324e300bc2a6b1e63d4d8f772633ae995824
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-02-19 12:52:42 +0100

    NextInstance.immutableAssetToken

--- test/lib/next-modes/base.ts
+++ test/lib/next-modes/base.ts
@@ -285,7 +285,7 @@ export class NextInstance {
                     ? // since we can't get the build id as a build artifact,
                       // add it in build logs
                       {
-                        'post-build': `node -e 'console.log("BUILD" + "_ID: " + fs.readFileSync("${this.distDir}/BUILD_ID") + "\\nDEPLOYMENT_ID: " + process.env.NEXT_DEPLOYMENT_ID)'`,
+                        'post-build': `node -e 'console.log("BUILD" + "_ID: " + fs.readFileSync("${this.distDir}/BUILD_ID") + "\\nDEPLOYMENT_ID: " + process.env.NEXT_DEPLOYMENT_ID + "\\nIMMUTABLE_ASSET_TOKEN: " + process.env.VERCEL_IMMUTABLE_ASSET_TOKEN)'`,
                       }
                     : {}),
                   ...pkgScripts,
@@ -644,8 +644,20 @@ export class NextInstance {
     return undefined
   }
 
-  public get deploymentIdQuery(): string {
-    return this.deploymentId ? `?dpl=${this.deploymentId}` : ''
+  public getDeploymentIdQuery(ampersand: boolean = false): string | undefined {
+    const prefix = ampersand ? '&' : '?'
+    return this.deploymentId ? `${prefix}dpl=${this.deploymentId}` : ''
+  }
+
+  public get immutableAssetToken(): string | undefined {
+    return undefined
+  }
+
+  public getAssetQuery(ampersand: boolean = false): string | undefined {
+    const prefix = ampersand ? '&' : '?'
+    return this.immutableAssetToken || this.deploymentId
+      ? `${prefix}dpl=${this.immutableAssetToken || this.deploymentId}`
+      : ''
   }
 
   public get cliOutput(): string {
